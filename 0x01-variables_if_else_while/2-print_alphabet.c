110c6f809345c6f79cf5a79417e70661ff9f7b9c
macisuman <macisuman@gmail.com>
2022-09-09 17:25:00 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - learning the hard way
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	char hamza = 'a';
+
+	while (hamza <= 'z')
+	{
+		put char(hamza);
+		hamza++;
+	}
+	putchar(10);
+	return (0);
+}
