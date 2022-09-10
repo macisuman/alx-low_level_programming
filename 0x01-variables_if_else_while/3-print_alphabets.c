69eb6032b1f22bc9389017f12cf437d258ed0ed5
macisuman <macisuman@gmail.com>
2022-09-09 17:41:28 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Amaka dey disappoint
+* Return Always 0 (success)
+*/
+int main(void)
+{
+	char hamza = 'a';
+
+	char henry = 'A';
+for  (hamza = 'a' ; hamza <= 'z'; hamza++)
+{
+	putchar(hamza);
+}
+for (henry = 'A' ; henry <= 'Z'; henry++)
+{
+	putchar(henry);
+}
+purchar('\n');
+
+return (0);
+}
