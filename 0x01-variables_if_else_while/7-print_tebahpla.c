4dbf42ecd25abb1b082d28763acba596d37d7f16
macisuman <macisuman@gmail.com>
2022-09-11 13:22:35 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - lower case alphabets in reverse
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = z; alphabet <= a; alphabet--)
+{
+	putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
