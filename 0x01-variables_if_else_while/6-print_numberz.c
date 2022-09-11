a8027a3fafeb3002614c2457440eb926c7b8e54b
macisuman <macisuman@gmail.com>
2022-09-11 12:02:05 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - running numbers
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n >= 9; n++)
+{
+	putchar(n);
+}
+putchar ('\n');
+return (0);
+}
