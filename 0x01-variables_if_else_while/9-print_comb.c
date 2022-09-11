2afee38244ad8b8b18b30d80b4a87d5fd3f6fc6a
macisuman <macisuman@gmail.com>
2022-09-11 14:41:31 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - print print single digit numbers
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+while (i < 58)
+{
+	putchar(i);
+	if (i != 57)
+{
+	putchar(44);
+	putchar(32);
+}
+i++;
+}
+putchar(10);
+return (0);
+}
