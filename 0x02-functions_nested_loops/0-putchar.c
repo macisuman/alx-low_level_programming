adf85d262dc6be04c0f7c694e7cf67ee496fadf3
macisuman <macisuman@gmail.com>
2022-09-13 15:27:59 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * main - C level programming
 * Return: Always 0 (success)
@@ -9,7 +10,7 @@ int main(void)
 	char i[9] = "_putchar";
 	int n = 0;
 
-	for (n = 0; i <= 9; i++)
+	for (n = 0; n <= 9; n++)
 {
 	putchar(i[n]);
 }
