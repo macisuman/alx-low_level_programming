337bdac7952260b6fc4187c857b01bb46b5fafa7
macisuman <macisuman@gmail.com>
2022-09-13 14:57:08 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - C level programming
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char i[9] = "_putchar";
+	int n = 0;
+
+	for (n = 0; i <= 9; i++)
+{
+	putchar(i[n]);
+}
+return (0);
+}
