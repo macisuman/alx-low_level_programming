3aab5c6f027fa49e3044de25c55ee4a7dcad279a
macisuman <macisuman@gmail.com>
2022-09-14 09:19:16 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* jack_bauer - function that prints 24 hours
+* h = hours, m = minutes
+* Return: 24 hour clock
+*/
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+{
+	for (m = 0; m < 60; m++)
+{
+	_putchar((h / 10) + '0');
+	_putchar((h % 10) + '0');
+	-putchar (':');
+	_putchar((m / 10) + '0');
+	_putchar((m % 10) + '0');
+	_putchar('\n');
+
+}
+}
+}
