cec3dba0943e52171773f7bb35a0fd2426f5de03
macisuman <macisuman@gmail.com>
2022-09-13 17:19:07 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _islower - if statement
+* @c: Na wa for betty sha
+* Return: always 0 (success)
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c >= 'z')
+{
+		return (1);
+}
+{
+	else
+		return (0);
+}
+}
