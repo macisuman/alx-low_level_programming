30e059fdf24cef767afe7a0de1fab0a74a12c9d3
macisuman <macisuman@gmail.com>
2022-09-13 18:24:11 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+* _abs - computes the value of an integer
+* @n - is an integer
+* main - please work
+* Return - always 0
+*/
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+{
+	return ('n');
+}
+else
+{
+	return ('n');
+}
+}
