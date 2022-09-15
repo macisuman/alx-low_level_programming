1ea70b1bb88375e7d7c2c4570bcb3062e9a46ee1
macisuman <macisuman@gmail.com>
2022-09-15 03:40:39 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stido.h.>
+#include "main.h"
+/**
+* print_to_98 - prints all integers to 98
+* @n: starting integer
+*/
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d, \n", n);
+			n--;
+		}
+	}
+	else if (n <= 98 && n > 0)
+	{
+		while (n <= 98 && n > 0)
+		{
+			printf("%d, \n", n);
+			n++;
+		}
+		else
+		{
+			printf("%d, \n", n);
+		}
+
+}
