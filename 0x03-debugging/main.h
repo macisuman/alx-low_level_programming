d5df32e55d5f172ea2c451fdc6a7ac18e184a2c9
macisuman <macisuman@gmail.com>
2022-09-15 11:59:27 -0700

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#include "main.h"
+#ifndef "MAIN_H"
+#include <stdio.h>
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
+
+largest_number(a, b, c);
+int largest_number(int a, int b, int c)
+print_remaining_days(month, day, year);
+convert_day(month, day);
+int convert_day(int month, int day)
+void print_remaining_days(int month, int day, int year)
+
