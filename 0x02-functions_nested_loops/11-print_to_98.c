e7f58424db23ad818ad61be777682c31c9154753
macisuman <macisuman@gmail.com>
2022-09-15 04:23:57 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,15 +12,17 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d, ", n++);
+			printf("%d, ", n);
+			n++;
 		}
 	}
 	else if
 	{
 		while (n > 98)
 		{
-			printf("%d, ", n--);
+			printf("%d, ", n);
+			n--;
 		}
 	}
-
+printf("98\n");
 }
