7ad972d8aa31cd654edb97fe6aade84432eec953
macisuman <macisuman@gmail.com>
2022-09-16 09:47:57 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 {
-	if (month >= 2 && day >= 60)
+	if (month > 2 && day >= 60)
 {
 	day++;
 }
@@ -32,4 +32,4 @@ else
 	printf("Remaining days: %d\n", 365 - day);
 }
 }
-}
+i}
