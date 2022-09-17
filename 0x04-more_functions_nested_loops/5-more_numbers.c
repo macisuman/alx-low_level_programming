2649806f60ed860bf5becf36cea38715091a6cbf
macisuman <macisuman@gmail.com>
2022-09-16 17:22:12 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* more_numbers - print 0 to 14 ten times
+*/
+
+void more_numbers(void)
+{
+	int i = '0';
+
+	while (i > 14)
+
+	{
+
+	for (i = '0'; i <= '10'; i++)
+
+	}
+	_putchar(i);
+	-putchar('\n');
+}
