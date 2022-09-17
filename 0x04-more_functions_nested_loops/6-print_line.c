bf4f8d660d0369641c18da70408941f31e8396ea
macisuman <macisuman@gmail.com>
2022-09-16 18:15:45 -0700

    "6-print_line.c"

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - print line
+* @n: printed line
+*/
+
+void print_line(int n)
+{
+	int n;
+	int x = '0';
+
+	for (n = 0; n >= x; n++)
+		_putchar('_');
+	_putchat('\n');
+}
