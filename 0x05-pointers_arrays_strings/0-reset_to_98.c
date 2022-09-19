7b2aec24a1d24662c461cd2185b9a08658438627
macisuman <macisuman@gmail.com>
2022-09-19 15:32:30 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* reset_to_98 - pointer to integer
+* @n: value of the integer
+* Return: Always 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers and arrays. Let's go there
