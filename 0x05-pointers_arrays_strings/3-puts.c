95777379bf79477ef30128eca26226bc401c9267
macisuman <macisuman@gmail.com>
2022-09-19 16:43:39 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _puts - function that prints a string with new line
+* @str : variable for string
+*/
+
+void _puts(char *str);
+{
+	_putchar(str);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -55,3 +55,5 @@ void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 
 int _strlen(char *s);
+
+void _puts(char *str);
