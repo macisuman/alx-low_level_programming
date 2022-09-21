b8a8c0e4045e4566beed6bc2c5d50fd56f1c5fc4
macisuman <macisuman@gmail.com>
2022-09-21 11:48:58 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+include "main.h"
+/**
+* _strcat - concatenante two strings
+* @dest: variable one
+* @src: variable two
+* Return: return 0
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int length, j;
+
+	length = 0;
+
+	while (*dest[length] != '\0')
+	{
+		++length;
+	}
+
+	for (j = 0; *src != '\0'; ++length)
+	{
+		*dest[length] = *src[j];
+	}
+
+	*dest[length] = '\0';
+
+	_putchar(*dest);
+
+	return (0);
+
+}
