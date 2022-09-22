54dbdaa7fc82d3abdb18d8bfaa03b624c8d7bd90
macisuman <110997734+macisuman@users.noreply.github.com>
2022-09-22 20:56:05 +0100

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @str: parameter of string to be changed
+ *
+ * Return: return a character
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
