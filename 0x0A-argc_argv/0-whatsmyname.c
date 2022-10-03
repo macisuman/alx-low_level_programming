9c6c15e4414ba9817408f40aecfd77c8a966e7d5
macisuman <110997734+macisuman@users.noreply.github.com>
2022-10-03 22:06:53 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - the main function
+ * @argv: the firs argument will be passed unto
+ * @argc: the number it will return
+ * Return: returns nothing
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
