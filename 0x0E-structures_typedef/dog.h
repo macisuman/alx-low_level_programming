afdf4e046d3e83a4c83c5f5f98f939247727c0b8
macisuman <macisuman@gmail.com>
2022-10-11 12:31:50 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,12 @@
-#ifndef "dog.h"
-#define "dog.h"
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
 * struct dog - structure being created
+* main- main function
 * @name: name of the dog
 * @age: age of the dog
 * @owner: name of the owner of the dog
+* Return - Return 0
 */
 
 struct dog
